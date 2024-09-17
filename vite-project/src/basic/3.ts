23ef4edc4dc349db448c6c8a6733b7c70506a1bb
Anna <iaanya27@gmail.com>
2024-09-17 13:36:25 +0300

    added task2 and 3

--- vite-project/src/basic/3.ts
+++ vite-project/src/basic/3.ts
@@ -1,2 +1,24 @@
 // Створіть змінну, яка може містити або рядок, або число(union type) ?
 // Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable'(literal type).
+
+
+
+
+// ********************union type***************
+let unionType: string | number;
+
+unionType = 'string'; // OK
+unionType = 10; // OK
+// unionType = true; // Error
+
+
+
+
+// ********************literal type***************
+
+type EnableOrDisable = 'enable' | 'disable';
+let answer: EnableOrDisable;
+
+answer = 'enable'; // ok
+answer = 'disable'; // ok
+// answer = 'sorry'; // Error
